bac3c07c146f8d2817c645a0663c31479e38fe09
Juan Pablo Castano <jpcastanom@gmail.com>
2021-07-23 10:47:17 -0500

    cloudinary, movie create and edit

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,3 +1,58 @@
+const Movie = require("./../models/Movie.model")
+
 exports.createMovie = async (req, res) => {
   res.render("movie-views/movie-create")
+}
+
+exports.processNewMovie = async (req, res) => {
+  const { title, description } = req.body
+
+  Movie.create({
+    title,
+    description,
+    imageUrl: req.file.path,
+  })
+    .then((movieCreated) => {
+      res.redirect("/movies")
+    })
+    .catch((e) => console.log("There was an error creating movie", e))
+}
+
+exports.listMovies = async (req, res) => {
+  Movie.find().then((foundMovies) => {
+    res.render("movie-views/movies-list", {
+      movies: foundMovies,
+    })
+  })
+}
+
+exports.editMovie = async (req, res) => {
+  const { id } = req.params
+
+  Movie.findById(id)
+    .then((movieFound) => {
+      res.render("movie-views/movie-edit", movieFound)
+    })
+    .catch((e) => console.log("Error finding movie to edit: ", e))
+}
+
+exports.updateMovie = async (req, res) => {
+  const { id } = req.params
+  const { title, description, existingImage } = req.body
+
+  console.log(existingImage)
+
+  let imageUrl
+
+  if (req.file) {
+    imageUrl = req.file.path
+  } else {
+    imageUrl = existingImage
+  }
+
+  Movie.findByIdAndUpdate(id, { title, description, imageUrl }, { new: true })
+    .then(() => {
+      res.redirect("/movies")
+    })
+    .catch((e) => console.log("Error editing movie: ", e))
 }
--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -2,8 +2,24 @@
 const express = require("express")
 const router = express.Router()
 const movieController = require("./../controllers/movieController")
+const fileUploader = require("./../config/cloudinary.config")
 
 /* ROUTE */
+router.get("/", movieController.listMovies)
 router.get("/create", movieController.createMovie)
 
+router.post(
+  "/create",
+  fileUploader.single("movie-cover-image"),
+  movieController.processNewMovie
+)
+
+router.get("/:id/edit", movieController.editMovie)
+router.post(
+  "/:id/edit",
+  fileUploader.single("movie-cover-image"),
+  movieController.updateMovie
+)
+
+/* EXPORTS */
 module.exports = router
